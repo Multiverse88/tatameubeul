4d01b97dee0578d9cf2549bc64cbfd2f259b5687
Multiverse88 <ainanihj@gmail.com>
2025-01-25 15:04:53 +0700

    update nav, footer

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,11 @@
 import { resolve } from 'path';
 import { defineConfig } from 'vite';
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  plugins: [
+    tailwindcss(),
+  ],  
   server: {
     port: 5173,
     open: '/index.html'
